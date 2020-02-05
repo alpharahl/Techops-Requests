e868a14259a761a5ed09089d9bfbf8ce5cd82d6b
David Gauntlett <david.m.gauntlett@gmail.com>
2020-02-04 20:03:00 -0500

    abstracting the questions even more

--- src/components/request/av/Sound.vue
+++ src/components/request/av/Sound.vue
@@ -1,46 +1,22 @@
 <template>
-  <div>
-    <b-form @submit.prevent class="d-flex justify-content-center mt-4">
-      <label>Do you need sound/microphones for your room?</label>
-      <b-form-radio-group
-        v-model="selected"
-        :options="options"
-        value-field="item"
-        text-field="name"
-        id="audioForm"
-        class="ml-5"
-        required
-        :disabled="!isOwner"
-      />
-    </b-form>
-    <div v-if="selected === 'no'">
-      Ok, we will not be providing any microphones and or speakers...
-    </div>
-    <div v-else-if="selected === 'yes'">
-      Please tell us what you are aiming to do with audio
-      <b-textarea
-        v-model="description"
-        :disabled="!isOwner"
-      />
-      <div class="d-flex">
-        <small class="ml-auto">{{descSaveStatus}}</small>
-      </div>
-    </div>
-  </div>
+  <Question
+    selectName="useAudio"
+    descName="audioDesc"
+    question="Do you need sound/microphones for your room?"
+    negativeStatement="Ok, we will not be providing any microphones or sound support"
+    informationReq="Please tell us what sound needs you have"
+    :isOwner="isOwner"
+    :av="av"
+  />
 </template>
 
 <script>
-  import avMixin from "@/components/request/av/avMixin";
+  import Question from "@/components/shared/question";
 
   export default {
     name: "Sound",
-    mixins: [avMixin],
-    data(){
-      return {
-        selectName: 'useAudio',
-        descName: 'audioDesc'
-      }
-    }
+    components: {Question},
+    props: ['av', 'isOwner'],
   }
 </script>
 
---
+++ src/components/shared/question.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div>
+    <b-form @submit.prevent class="d-flex justify-content-center mt-4">
+      <label>{{question}}</label>
+      <b-form-radio-group
+        v-model="selected"
+        :options="options"
+        value-field="item"
+        text-field="name"
+        id="audioForm"
+        class="ml-5"
+        required
+        :disabled="!isOwner"
+      />
+    </b-form>
+    <div v-if="selected === 'no'">
+      {{negativeStatement}}
+    </div>
+    <div v-else-if="selected === 'yes'">
+      {{informationReq}}
+      <b-textarea
+        v-model="description"
+        :disabled="!isOwner"
+      />
+      <div class="d-flex">
+        <small class="ml-auto">{{descSaveStatus}}</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import avMixin from "@/components/shared/avMixin";
+
+  export default {
+    name: "question",
+    mixins: [avMixin],
+    props: [
+      'selectName',
+      'descName',
+      'question',
+      'negativeStatement',
+      'informationReq'
+    ],
+  }
+</script>
+
+<style scoped>
+
+</style>
